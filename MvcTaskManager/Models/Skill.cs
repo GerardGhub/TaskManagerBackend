8c62cae1babc1b9f85cf93dac77477152cadebc5
Gerard Singian <singian28@gmail.com>
2024-06-07 01:48:56 +0800

    Sumak Update()

--- MvcTaskManager/Models/Skill.cs
+++ MvcTaskManager/Models/Skill.cs
@@ -1,10 +1,6 @@
 ﻿using MvcTaskManager.Identity;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MvcTaskManager.Models
 {
--- MvcTaskManager/Startup.cs
+++ MvcTaskManager/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -12,9 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MvcTaskManager.Identity;
 using MvcTaskManager.ServiceContracts;
 using MvcTaskManager.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MvcTaskManager
 {
